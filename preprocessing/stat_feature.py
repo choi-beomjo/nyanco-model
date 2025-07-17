c1a8162741570222172296b24131a15eae58319f
beomjo <bbjj009911@gmail.com>
2025-07-17 16:17:35 +0900

    refactor: 최종 모델 학습 main으로 이동 및 함수 추출 (#2)

--- preprocessing/stat_feature.py
+++ preprocessing/stat_feature.py
@@ -11,32 +11,38 @@ stat_features = [
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
-scaler = MinMaxScaler()
+def get_normalize_scaler(character_df):
 
-character_df = pd.read_csv('../db/characters.csv')
+    scaler = MinMaxScaler()
 
-normalized_stats = scaler.fit_transform(character_df[stat_features])
+    # character_df = pd.read_csv('../db/characters.csv')
 
-# character_df에는 character_id 컬럼이 있다고 가정
-character_id_list = character_df["id"].tolist()
+    normalized_stats = scaler.fit_transform(character_df[stat_features])
+    joblib.dump(scaler, "stat_scaler.pkl")
 
-# character_id -> normalized vector
-char_stat_dict = {
-    char_id: normalized_stats[i]
-    for i, char_id in enumerate(character_id_list)
-}
+    # character_df에는 character_id 컬럼이 있다고 가정
+    character_id_list = character_df["id"].tolist()
 
-train_df = pd.read_csv("stage_character_train.csv")
+    # character_id -> normalized vector
+    char_stat_dict = {
+        char_id: normalized_stats[i]
+        for i, char_id in enumerate(character_id_list)
+    }
 
-# stat vector 붙이기
-stat_features_df = pd.DataFrame(
-    train_df["character_id"].map(char_stat_dict).tolist(),
-    columns=[f"stat_{f}" for f in stat_features]
-)
+    return char_stat_dict
 
-# 붙이기
-train_df = pd.concat([train_df, stat_features_df], axis=1)
+# train_df = pd.read_csv("stage_character_train.csv")
 
-train_df.to_csv("deep_train_with_stats.csv", index=False)
-joblib.dump(scaler, "stat_scaler.pkl")
+def make_stat_vector(train_df, char_stat_dict):
+    # stat vector 붙이기
+    stat_features_df = pd.DataFrame(
+        train_df["character_id"].map(char_stat_dict).tolist(),
+        columns=[f"stat_{f}" for f in stat_features]
+    )
+
+    # 붙이기
+    train_df = pd.concat([train_df, stat_features_df], axis=1)
+
+    train_df.to_csv("deep_train_with_stats.csv", index=False)
+
 
--- train/train_deep.py
+++ train/train_deep.py
@@ -1,80 +1,13 @@
 import numpy as np
 import pandas as pd
 import torch
-from model.deep_rec_model import DeepRecModel
 import torch.nn as nn
-
-
-train_df = pd.read_csv('../preprocessing/deep_train_with_stats.csv')
-
-embeddings = torch.load("../graph/full_embeddings.pt")
-node_mapping = torch.load("../graph/node_mapping_full.pt")
-
-stage_id_to_idx = node_mapping["stage_id_to_idx"]
-char_id_to_idx = node_mapping["character_id_to_idx"]
-
-import joblib
-
-# 필요한 파일들 로드
-character_df = pd.read_csv('../db/characters.csv')
-scaler = joblib.load("../preprocessing/stat_scaler.pkl")  # 정규화용 scaler 불러오기
-
-stat_features = [
-    "hp", "atk1", "atk2", "atk3",
-    "pre_atk1", "pre_atk2", "pre_atk3",
-    "back_atk", "atk_freq",
-    "range", "long_distance1", "long_distance2",
-    "cost", "kb", "speed", "spawn"
-]
-
-# 정규화
-normalized_stats = scaler.transform(character_df[stat_features])
-
-# character_id ↔ normalized vector 매핑
-character_id_list = character_df["id"].tolist()
-char_stat_dict = {
-    char_id: normalized_stats[i]
-    for i, char_id in enumerate(character_id_list)
-}
-
-
-deep_input_vectors = []
-
-for _, row in train_df.iterrows():
-    stage_id = row["stage_id"]
-    character_id = row["character_id"]
-
-    stage_emb = embeddings[stage_id_to_idx[stage_id]]  # (64,)
-    char_emb = embeddings[char_id_to_idx[character_id]]  # (64,)
-    stat_vec = char_stat_dict[character_id]  # (16,)
-
-    # 순서 중요: [stage, character, stat]
-    input_vec = np.concatenate([stage_emb, char_emb, stat_vec])  # (144,)
-    deep_input_vectors.append(input_vec)
-
-X = np.array(deep_input_vectors)
-y = train_df["label"].values
-
 from sklearn.model_selection import train_test_split
+import joblib
+from torch.utils.data import TensorDataset, DataLoader
 
-# 스테이지 아이디를 기준으로 데이터 분류
-all_stage_ids = train_df["stage_id"].unique()
 
-# 1차: train + temp
-stage_train, stage_temp = train_test_split(
-    all_stage_ids, test_size=0.3, random_state=42
-)
-
-# 2차: validation + test
-stage_val, stage_test = train_test_split(
-    stage_temp, test_size=0.5, random_state=42
-)
-
-df_train = train_df[train_df["stage_id"].isin(stage_train)]
-df_val   = train_df[train_df["stage_id"].isin(stage_val)]
-df_test = train_df[train_df["stage_id"].isin(stage_test)]
-
-def build_X_y(df):
+def build_X_y(df, embeddings, stage_id_to_idx, char_id_to_idx, char_stat_dict):
     X = []
     for _, row in df.iterrows():
         stage_id = row["stage_id"]
@@ -91,53 +24,77 @@ def build_X_y(df):
     return X, y
 
 # 실행
-X_train, y_train = build_X_y(df_train)
-X_val, y_val = build_X_y(df_val)
-X_test, y_test = build_X_y(df_test)
+
+def set_data_train_val_by_stage(train_df, embeddings, stage_id_to_idx, char_id_to_idx, char_stat_dict):
+    # 스테이지 아이디를 기준으로 데이터 분류
+    all_stage_ids = train_df["stage_id"].unique()
+
+    # 1차: train + temp
+    stage_train, stage_temp = train_test_split(
+        all_stage_ids, test_size=0.3, random_state=42
+    )
+
+    # 2차: validation + test
+    stage_val, stage_test = train_test_split(
+        stage_temp, test_size=0.5, random_state=42
+    )
+
+    df_train = train_df[train_df["stage_id"].isin(stage_train)]
+    df_val = train_df[train_df["stage_id"].isin(stage_val)]
+    df_test = train_df[train_df["stage_id"].isin(stage_test)]
 
 
+    X_train, y_train = build_X_y(df_train, embeddings, stage_id_to_idx, char_id_to_idx, char_stat_dict)
+    X_val, y_val = build_X_y(df_val, embeddings, stage_id_to_idx, char_id_to_idx, char_stat_dict)
+    X_test, y_test = build_X_y(df_test, embeddings, stage_id_to_idx, char_id_to_idx, char_stat_dict)
 
-from torch.utils.data import TensorDataset, DataLoader
 
-train_dataset = TensorDataset(torch.tensor(X_train, dtype=torch.float32),
-                              torch.tensor(y_train, dtype=torch.float32))
-val_dataset = TensorDataset(torch.tensor(X_val, dtype=torch.float32),
-                            torch.tensor(y_val, dtype=torch.float32))
+    train_dataset = TensorDataset(torch.tensor(X_train, dtype=torch.float32),
+                                  torch.tensor(y_train, dtype=torch.float32))
+    val_dataset = TensorDataset(torch.tensor(X_val, dtype=torch.float32),
+                                torch.tensor(y_val, dtype=torch.float32))
+    test_dataset = TensorDataset(torch.tensor(X_test, dtype=torch.float32),
+                                 torch.tensor(y_test, dtype=torch.float32))
 
-train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=256)
+    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=256)
+    test_loader = DataLoader(test_dataset, batch_size=256)
 
-model = DeepRecModel()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-loss_fn = nn.BCELoss()
+    return train_loader, val_loader, test_loader
 
-for epoch in range(1, 21):
-    model.train()
-    total_loss = 0
-    for xb, yb in train_loader:
-        optimizer.zero_grad()
-        preds = model(xb).squeeze()
-        loss = loss_fn(preds, yb)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
 
-    # 검증 단계
-    model.eval()
-    val_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for xb, yb in val_loader:
-            pred = model(xb).squeeze()
-            loss = loss_fn(pred, yb)
-            val_loss += loss.item()
-            predicted = (pred >= 0.5).float()
-            correct += (predicted == yb).sum().item()
-            total += yb.size(0)
+def train_loop(model, epochs, train_loader, val_loader):
+# model = DeepRecModel()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    loss_fn = nn.BCELoss()
 
-    acc = correct / total
+    for epoch in range(1, epochs):
+        model.train()
+        total_loss = 0
+        for xb, yb in train_loader:
+            optimizer.zero_grad()
+            preds = model(xb).squeeze()
+            loss = loss_fn(preds, yb)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
 
-    print(f"[Epoch {epoch}] Loss: {total_loss:.4f} | Val Loss: {val_loss:.4f} | Val Acc: {acc:.4f}")
+        # 검증 단계
+        model.eval()
+        val_loss = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for xb, yb in val_loader:
+                pred = model(xb).squeeze()
+                loss = loss_fn(pred, yb)
+                val_loss += loss.item()
+                predicted = (pred >= 0.5).float()
+                correct += (predicted == yb).sum().item()
+                total += yb.size(0)
 
-torch.save(model, "deeprec_model.pt")
+        acc = correct / total
+
+        print(f"[Epoch {epoch}] Loss: {total_loss:.4f} | Val Loss: {val_loss:.4f} | Val Acc: {acc:.4f}")
+
+    torch.save(model, "deeprec_model.pt")
